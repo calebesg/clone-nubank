8a7e382ef4e79e8673e33bdad64c9da8a105e6c2
CalebeSG <calebeguimaraes10@gmail.com>
2020-08-24 18:11:23 -0300

    create card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 23.400,00</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferencia de R$ 20,00 recebida de Fulano as 06:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
